5d56488e8f6f2928fed60344fda69c0acecdae32
Elena <eurogarage_elena@mail.ru>
2022-03-27 09:56:30 +0300

    first learn

--- qw.js
+++ qw.js
@@ -1,1 +1,17 @@
-const q1=10;
+"use strict";
+const numberOfFilms = +prompt ("Сколько фильмов вы уже посмотрели?", " ");
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies:{},
+   actors:{},
+   genres:[],
+   privat:false
+};
+const a = prompt ('Один из последних просмотренных фильмов?', ' '),
+      b = prompt ('На сколько оцените его?',' '),
+      c = prompt ('Один из последних просмотренных фильмов?', ' '),
+      d = prompt ('На сколько оцените его?',' ');
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
+//  нам выдаст a:b , тоесть название фильма и присвоенный ему рейтинг
